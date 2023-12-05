ced19b83a6e492afc42a82f37e34aa459f553999
Buse Eşer <buseesher@gmail.com>
2023-12-05 21:02:27 +0300

    Add project files.

---
+++ ConfigurationManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace Singleton
+{
+    public class ConfigurationService
+    { 
+        public string GetValue(string key)
+        {
+            ConfigurationManager configurationManager = new();
+            string path= Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            configurationManager.SetBasePath(path);
+            configurationManager.AddJsonFile("Configuration.json");
+            return configurationManager.GetSection(key).Value;  
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Singleton;
+
+Console.WriteLine("Singleton");
+ConfigurationService configurationService = new();
+var azureConnectionString = configurationService.GetValue("Storage:Azure");
+
+Console.WriteLine();
